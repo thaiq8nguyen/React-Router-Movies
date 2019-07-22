3141d1b1ba26deedff35e9fcda96597c42416f10
Thai Nguyen <discoverylight@yahoo.com>
2019-07-22 12:39:08 -0700

    mvp complete

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Link } from "react-router-dom";
 const MovieList = props => {
   const [movies, setMovies] = useState([])
   useEffect(() => {
@@ -20,7 +20,7 @@ const MovieList = props => {
   
   return (
     <div className="movie-list">
-      {this.state.movies.map(movie => (
+      {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -28,9 +28,10 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  const { id, title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <Link to={`/movies/${id}`}>
+      <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -46,6 +47,8 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+    </Link>
+    
   );
 }
 
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    {/* <div className="home-button">Home</div> */}
+    <Link to="/" className="home-button">Home</Link>
   </div>
 );
 
